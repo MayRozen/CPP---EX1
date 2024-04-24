9643f7cc6e3d49d9cc8e86d90dd611cf5497f8a0
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-04-24 16:59:22 +0300

    3

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -4,24 +4,22 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include "Graph.hpp"
 
 class Algorithms{ // our namespace
-    public:
-
+public:
     // return 1 if the graph is a 'connected graph' and 0 else
     int isConnected(Graph g);
 
     // return the shortest path between 'start' ans 'end', else -> -1.
-    vector<int> shortestPath(Graph g,int start,int end);
+    std::vector<int> shortestPath(Graph g,int start,int end);
 
     // Prints a circle if exist
     void isContainsCycle(Graph g);
 
-
-    isBipartite(Graph g);
+    // return the partition of the graph in a bipartite graph
+    std::vector<int> isBipartite(Graph g);
 
     // return a vector which include the Vertices of a negative circle if exist. If not -> return 0.
-    vector<int> negativeCycle(Graph g);
-}
-
-
+    std::vector<int> negativeCycle(Graph g);
+};
