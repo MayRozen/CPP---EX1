fdbd369659c13e31728ba114e27e00142f8a9aa4
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-04-24 11:37:17 +0300

    1

---
+++ Algorithms.hpp
@@ -0,0 +1,15 @@
+/*  ID: 212051007
+    Mail: mayrozen45@gmail.com
+*/
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+int isConnected(Graph g);
+
+maslul shortestPath(Graph g,start,end);
+isContainsCycle(g)
+isBipartite(g)
+negativeCycle(g)
+
+
