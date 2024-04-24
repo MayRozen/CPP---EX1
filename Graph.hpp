3624d462ec71b15a8d863f6fb9302924cb5ff326
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-04-24 11:42:41 +0300

    1

---
+++ Graph.hpp
@@ -0,0 +1,21 @@
+/*  ID: 212051007
+    Mail: mayrozen45@gmail.com
+*/
+
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+
+class Graph {
+private:
+    int numVertices;
+    std::vector<std::vector<int>> adjMatrix;
+
+public:
+    Graph(int n); // constructor
+    void loadGraph(const std::vector<std::vector<int>>& adjMat);
+    void printGraph();
+};
+
+#endif // GRAPH_HPP
