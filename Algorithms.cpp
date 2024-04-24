773ce5cae124e1f0e24221b1cd819cfb5e1c9b14
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-04-24 17:12:22 +0300

    Update Algorithms.cpp

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -7,34 +7,38 @@
 #include "Graph.hpp"
 #include "Algorithms.hpp"
 
-namespace Algorithems{
-    public:
-        int isConnected(Graph g){
-            int numVertices = g.adjMatrix.size();
-            std::vector<bool> visited(numVertices, false);
+class Algorithems{
+public:
+    int isConnected(Graph g){
+        int numVertices = g.adjMatrix.size();
+        std::vector<bool> visited(numVertices, false);
             
-            // Perform DFS traversal from the first vertex
-            dfs(0, visited);
+        // Perform DFS traversal from the first vertex
+        dfs(0, visited);
 
-            // Check if all vertices were visited
-            for (bool v : visited) {
-                if (!v) {
-                    return false; // Graph is not connected
-                }
-            }
-            return true; // if 'false' were not return -> Graph is connected
-        }
-
-    private:
-        // Depth-first search (DFS)
-        void dfs(int v, std::vector<bool>& visited) {
-            visited[v] = true;
-            for (int i = 0; i < g.adjMatrix[v].size(); ++i) {
-                if (adjMatrix[v][i] && !visited[i]) {
-                    dfs(i, visited);
-                }
+        // Check if all vertices were visited
+        for (bool v : visited) {
+            if (!v) {
+                return false; // Graph is not connected
             }
         }
+        return true; // if 'false' were not return -> Graph is connected
+    }
+
+    std::vector<int> shortestPath(Graph g,int start,int end){
+        
+    }
+
+private:
+    // Depth-first search (DFS)
+    void dfs(int v, std::vector<bool>& visited) {
+        visited[v] = true;
+        for (int i = 0; i < adjMatrix[v].size(); ++i) {
+            if (adjMatrix[v][i] && !visited[i]) {
+                dfs(i, visited);
+            }
+        }
+    }
 }
 
 
