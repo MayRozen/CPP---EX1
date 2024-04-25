01c4131d9e626c0b26c043d26b2c7f5979839b7a
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-04-25 09:58:13 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -10,6 +10,16 @@ Graph::Graph(int n) : numVertices(n) {
     adjMatrix.resize(n, std::vector<int>(n, 0));
 }
 
+// getter to numVertices
+int Graph::getNumVertices() const{
+    return numVertices;
+}
+
+// getter to adjMatrix
+std::vector<std::vector<int>> Graph::getAdjMatrix() const{
+    return adjMatrix;
+}
+
 void Graph::loadGraph(const std::vector<std::vector<int>>& adjMat) {
     if (adjMat.size() != numVertices || adjMat[0].size() != numVertices) {
         std::cout << "Error: Incorrect matrix." << std::endl;
