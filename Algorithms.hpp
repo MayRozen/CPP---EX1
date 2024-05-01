75b91a47f0882259f443aae98207d68f99e09c79
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-01 21:51:21 +0300

    2

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -7,21 +7,21 @@
 #include "Graph.hpp"
 
 namespace ariel {
-class Algorithms{ // our namespace
-public:
-    // return 1 if the graph is a 'connected graph' and 0 else
-    bool isConnected(Graph g);
+    class Algorithms{ // our namespace
+    public:
+        // return 1 if the graph is a 'connected graph' and 0 else
+        bool isConnected(Graph g);
 
-    // return the shortest path between 'start' ans 'end', else -> -1.
-    std::vector<int> shortestPath(Graph g,int start,int end);
+        // return the shortest path between 'start' ans 'end', else -> -1.
+        std::vector<int> shortestPath(Graph g,int start,int end);
 
-    // Prints a circle if exist, 0 else
-    int isContainsCycle(Graph g);
+        // Prints a circle if exist, 0 else
+        int isContainsCycle(Graph g);
 
-    // return the partition of the graph in a bipartite graph. else -> print that there is no negative circle
-    std::vector<int> isBipartite(Graph g);
+        // return the partition of the graph in a bipartite graph. else -> print that there is no negative circle
+        std::vector<int> isBipartite(Graph g);
 
-    // return a vector which include the Vertices of a negative circle if exist. If not -> return 0.
-    std::vector<int> negativeCycle(Graph g);
-};
+        // return a vector which include the Vertices of a negative circle if exist. If not -> return 0.
+        std::vector<int> negativeCycle(Graph g);
+    };
 }
