fd033b89c57ba066391219863b7e44df643e2287
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-10 11:13:00 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -2,20 +2,21 @@
     Mail: mayrozen45@gmail.com
 */
 #include "doctest.h"
-#include "Algorithms.hpp"
-#include "Graph.hpp"
-
 using namespace std;
+using namespace Algorithms;
+using ariel::Graph;
+#include "Graph.hpp"
+#include "Algorithms.hpp"
 
 TEST_CASE("Test isConnected")
 {
-    ariel::Graph g;
+    Graph g;
     vector<vector<int>> graph = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
     g.loadGraph(graph);
-    CHECK(ariel::Algorithms::isConnected(g) == true);
+    CHECK(isConnected(g) == true);
 
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
@@ -24,18 +25,19 @@ TEST_CASE("Test isConnected")
         {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
-    CHECK(ariel::Algorithms::isConnected(g) == false);
+    CHECK(isConnected(g) == false);
 }
 
 TEST_CASE("Test shortestPath")
 {
-    ariel::Graph g;
+    Graph g;
     vector<vector<int>> graph = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
     g.loadGraph(graph);
-    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+    vector<int> ans = {0,1,2};
+    CHECK(shortestPath(g, 0, 2) == ans);
 
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
@@ -44,17 +46,18 @@ TEST_CASE("Test shortestPath")
         {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
-    CHECK(ariel::Algorithms::shortestPath(g, 0, 4) == "-1");
+    vector<int> ans = {-1};
+    CHECK(shortestPath(g, 0, 4) == ans);
 }
 TEST_CASE("Test isContainsCycle")
 {
-    ariel::Graph g;
+    Graph g;
     vector<vector<int>> graph = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
     g.loadGraph(graph);
-    CHECK(ariel::Algorithms::isContainsCycle(g) == false);
+    CHECK(isContainsCycle(g) == false);
 
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
@@ -63,17 +66,17 @@ TEST_CASE("Test isContainsCycle")
         {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
-    CHECK(ariel::Algorithms::isContainsCycle(g) == true);
+    CHECK(isContainsCycle(g) == true);
 }
 TEST_CASE("Test isBipartite")
 {
-    ariel::Graph g;
+    Graph g;
     vector<vector<int>> graph = {
         {0, 1, 0},
         {1, 0, 1},
         {0, 1, 0}};
     g.loadGraph(graph);
-    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+    CHECK(isBipartite(g) == 1);
 
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
@@ -82,7 +85,7 @@ TEST_CASE("Test isBipartite")
         {0, 0, 1, 0, 0},
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
-    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+    CHECK(isBipartite(g) == 0);
 
     vector<vector<int>> graph3 = {
         {0, 1, 2, 0, 0},
@@ -91,11 +94,11 @@ TEST_CASE("Test isBipartite")
         {0, 0, 4, 0, 5},
         {0, 0, 0, 5, 0}};
     g.loadGraph(graph3);
-    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2, 4}, B={1, 3}");
+    CHECK(isBipartite(g) == 1);
 }
 TEST_CASE("Test invalid graph")
 {
-    ariel::Graph g;
+    Graph g;
     vector<vector<int>> graph = {
         {0, 1, 2, 0},
         {1, 0, 3, 0},
