a3ba461b62c0f056572021fc941f9e222e54f3b9
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-10 11:12:33 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -5,19 +5,25 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include <iostream>
 #include <vector>
 
 namespace ariel {
     class Graph { // our namespace
-        Graph::Graph(int n) : numVertices(n) {
-        adjMatrix.resize(n, std::vector<int>(n, 0));
-        }
     private:
         int numVertices;
         std::vector<std::vector<int>> adjMatrix;
 
     public:
-        Graph(int n); // constructor
+
+        // Constructor taking the number of vertices
+        Graph() : numVertices(), adjMatrix(0, std::vector<int>(0, 0)) {} // empty graph
+
+        // Constructor taking the adjacency matrix
+        Graph(const std::vector<std::vector<int>>& Mat) : adjMatrix(Mat) {
+            numVertices = Mat.size();
+        }
+
         int getNumVertices() const;
         std::vector<std::vector<int>> getAdjMatrix() const;
         void loadGraph(const std::vector<std::vector<int>>& adjMat);
