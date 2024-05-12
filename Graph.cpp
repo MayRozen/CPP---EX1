22272037f3cb50db967bef1bdbeb5731a72e1835
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-12 11:21:01 +0300

    Update compilation errors

--- Graph.cpp
+++ Graph.cpp
@@ -17,24 +17,20 @@ std::vector<std::vector<int>> Graph::getAdjMatrix() const{
     return adjMatrix;
 }
 
+// setter to adjMatrix
 void Graph::loadGraph(const std::vector<std::vector<int>>& adjMat) {
-    if (adjMat.size() != numVertices || adjMat[0].size() != numVertices) {
-        std::cout << "Error: Incorrect matrix." << std::endl;
-        return;
-    }
-
-    adjMatrix = adjMat;
+    this->adjMatrix = adjMat;
+    this->numVertices = adjMat.size();
 }
 
-void Graph::printGraph() {
-    std::vector<std::vector<int>>::size_type edges = 0;
-    for (std::vector<int>::size_type i = 0; i < numVertices; ++i) {
-        for (std::vector<int>::size_type j = 0; j < numVertices; ++j) {
+void Graph::printGraph(){
+    int edges = 0;
+    for (IndexType i = 0; i < numVertices; i++) {
+        for (IndexType j = 0; j < numVertices; j++) {
             if(adjMatrix[i][j]!=0){ // Assuming the lack of an edge is expressed by 0
                 edges++;
             }
         }
-        std::cout << std::endl;
     }
     std::cout << "Graph with "<<numVertices<<" vertices and "<<edges<<" edges." << std::endl;
 }
