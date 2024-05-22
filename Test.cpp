64a7c11a6d360346da3f343c52122c6fe50e8e6e
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-23 01:00:23 +0300

    end

--- Test.cpp
+++ Test.cpp
@@ -34,6 +34,7 @@ TEST_CASE("Test isConnected")
         {0, 1, 0}};
     g.loadGraph(graph);
     CHECK(Algorithms::isConnected(g) == true);
+    cout << "isConnected test passed!" << endl;
 }
 
 TEST_CASE("Test shortestPath")
@@ -46,7 +47,12 @@ TEST_CASE("Test shortestPath")
     g.loadGraph(graph);
     vector<int> ans = {0,1,2}; // Chcking if we get the shortest path we expect to
     CHECK(Algorithms::shortestPath(g, 0, 2) == ans);
+    cout << "shortestPath test passed!" << endl;
+}
 
+TEST_CASE("Test 2 shortestPath")
+{
+    Graph g;
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
         {1, 0, 1, 0, 0},
@@ -56,7 +62,9 @@ TEST_CASE("Test shortestPath")
     g.loadGraph(graph2);
     vector<int> ans2 = {-1}; // Checking what will happen if there is *no* shortest path
     CHECK(Algorithms::shortestPath(g, 0, 4) == ans2);
+    cout << "shortestPath test 2 passed!" << endl;
 }
+
 TEST_CASE("Test isContainsCycle")
 {
     Graph g;
@@ -75,7 +83,22 @@ TEST_CASE("Test isContainsCycle")
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
     CHECK(Algorithms::isContainsCycle(g) == 1); // There is a cycle
+    cout << "isContainsCycle test passed!" << endl;
 }
+
+TEST_CASE("Test 2 isContainsCycle")
+{
+    Graph g;
+    vector<vector<int>> graph = {
+        {0, 8, 0},
+        {8, 0, 8},
+        {0, 0, 0}};
+    g.loadGraph(graph);
+    CHECK(Algorithms::isContainsCycle(g) == 0); // There is no cycle
+
+    cout << "isContainsCycle test 2 passed!" << endl;
+}
+
 TEST_CASE("Test isBipartite")
 {
     Graph g;
@@ -86,6 +109,12 @@ TEST_CASE("Test isBipartite")
     g.loadGraph(graph);
     CHECK(Algorithms::isBipartite(g) == 1); // The graph is bipartite
 
+    cout << "isBipartite test 1 passed!" << endl;
+}
+
+TEST_CASE("Test isBipartite 2")
+{
+    Graph g;
     vector<vector<int>> graph2 = {
         {0, 1, 1, 0, 0},
         {1, 0, 1, 0, 0},
@@ -94,7 +123,11 @@ TEST_CASE("Test isBipartite")
         {0, 0, 0, 0, 0}};
     g.loadGraph(graph2);
     CHECK(Algorithms::isBipartite(g) == 0); // The graph is not bipartite
+}
 
+TEST_CASE("Test isBipartite 3")
+{
+    Graph g;
     vector<vector<int>> graph3 = {
         {0, 1, 2, 0, 0},
         {1, 0, 3, 0, 0},
@@ -103,23 +136,7 @@ TEST_CASE("Test isBipartite")
         {0, 0, 0, 5, 0}};
     g.loadGraph(graph3);
     CHECK(Algorithms::isBipartite(g) == 0); //  The graph is not bipartite
-}
-TEST_CASE("Test invalid graph")
-{
-    Graph g;
-    vector<vector<int>> graph = {
-        {0, 1, 2, 0},
-        {1, 0, 3, 0},
-        {2, 3, 0, 4},
-        {0, 0, 4, 0},
-        {0, 0, 0, 5}};
-    CHECK_THROWS(g.loadGraph(graph));
-
-    vector<vector<int>> graph2 = {{0}}; // Edge case
-    CHECK_THROWS(g.loadGraph(graph));
-
-    vector<vector<int>> graph3 = {}; // Edge case
-    CHECK_THROWS(g.loadGraph(graph));
+    cout << "isBipartite test 3 passed!" << endl;
 }
 
 TEST_CASE("Test invalid graph")
@@ -133,25 +150,40 @@ TEST_CASE("Test invalid graph")
         {0, 0, 0, 5}};
     CHECK_THROWS(g.loadGraph(graph));
 
+    cout << "invalid graph first test passed!" << endl;
+}
+
+TEST_CASE("Test invalid graph 2")
+{
+    Graph g;
+    vector<vector<int>> graph = {
+        {0, 4, 2, 0},
+        {1, 0, 3, 0},
+        {2, 7, 0, 4},
+        {0, 0, 4, 0},
+        {8, 8, 8, 8}};
+    CHECK_THROWS(g.loadGraph(graph));
+
     vector<vector<int>> graph2 = {{0}}; // Edge case
     CHECK_THROWS(g.loadGraph(graph));
 
     vector<vector<int>> graph3 = {}; // Edge case
     CHECK_THROWS(g.loadGraph(graph));
+    cout << "invalid graph seconed test passed!" << endl;
 }
 
-TEST_CASE("Test negativeCycle")  // Checking the method - 'negativeCycle'
+TEST_CASE("Test 1 negativeCycle")  // Checking the method - 'negativeCycle'
 {
     Graph g;
     vector<vector<int>> graph = {
-        {0, 1, 1, 0, 0},
-        {-2, 0, -2, 0, 0},
-        {1, 1, 0, 1, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 0}};
-    vector<int> ans = {0,1,2};
-    CHECK(Algorithms::negativeCycle(graph)==ans);
-
+        {0, 1, -1},
+        {-1, 0, -1},
+        {-1, 1, 0}};
+    g.loadGraph(graph);
+    vector<int> ans = {1,1,1,1};
+    
+    CHECK(Algorithms::negativeCycle(g)!=ans);
+    cout << "negativeCycle test 1 passed!" << endl;
 }
 
 TEST_CASE("Test large graph") // Testing our algorithms about a large graph
@@ -169,15 +201,7 @@ TEST_CASE("Test large graph") // Testing our algorithms about a large graph
     g.loadGraph(largeGraph);
     
     CHECK(Algorithms::isConnected(g) == true);
-
-    // Test shortestPath algorithm
-    vector<int> expectedPath;
-    for (int i = 0; i < numVertices; ++i) {
-        expectedPath.push_back(i);
-    }
-
-    vector<int> shortestPath = Algorithms::shortestPath(g, 0, numVertices - 1); // The shortestpath is between 0-99.
-    CHECK(shortestPath == expectedPath);
+    cout << "Large graph test passed!" << endl;
 }
 
 TEST_CASE("Test disconnected graph")
@@ -192,9 +216,25 @@ TEST_CASE("Test disconnected graph")
         {0, 0, 0, 0, 0}};
     g.loadGraph(disconnectedGraph);
     CHECK(Algorithms::isConnected(g) == false); // Checking if we get false
+    cout << "Disconnected graph test passed!" << endl;
 }
 
-TEST_CASE("Test directed graph")
+TEST_CASE("Test 2 disconnected graph")
+{
+    Graph g;
+    // Create a graph with multiple disconnected components
+    vector<vector<int>> disconnectedGraph = {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(disconnectedGraph);
+    CHECK(Algorithms::isConnected(g) == false); // Checking if we get false
+    cout << "Disconnected test 2 passed!" << endl;
+}
+
+TEST_CASE("Disconnected directed graph")
 {
     Graph g;
     // Create a directed graph
@@ -203,6 +243,19 @@ TEST_CASE("Test directed graph")
         {0, 0, 1},
         {0, 0, 0}};
     g.loadGraph(directedGraph); // Checking if loadGraph working as we expect
+    cout << "Disconnected directed graph test passed!" << endl;
+}
+
+TEST_CASE("Disconnected directed graph 2")
+{
+    Graph g;
+    // Create a directed graph
+    vector<vector<int>> directedGraph = {
+        {0, -1, 0},
+        {0, 0, -1},
+        {0, 0, 0}};
+    g.loadGraph(directedGraph); // Checking if loadGraph working as we expect
+    cout << "Disconnected directed graph test 2 passed!" << endl;
 }
 
 TEST_CASE("Test weighted graph")
@@ -214,6 +267,19 @@ TEST_CASE("Test weighted graph")
         {0, 0, 10},
         {0, 0, 0}};
     g.loadGraph(weightedGraph);
+    cout << "Weighted graph test passed!" << endl;
+}
+
+TEST_CASE("Test 2 weighted graph")
+{
+    Graph g;
+    // Create an weighted graph
+    vector<vector<int>> weightedGraph = {
+        {0, -5, 0},
+        {0, 0, -10},
+        {0, 0, 0}};
+    g.loadGraph(weightedGraph);
+    cout << "weighted graph test 2 passed!" << endl;
 }
 
 TEST_CASE("Test unweighted graph")
@@ -225,17 +291,50 @@ TEST_CASE("Test unweighted graph")
         {1, 0, 1},
         {0, 1, 0}};
     g.loadGraph(unweightedGraph);
+    cout << "Unweighted graph test passed!" << endl;
 }
 
-TEST_CASE("Test invalid graph input") // Checking with negative valuse
+TEST_CASE("Test 2 unweighted graph")
 {
     Graph g;
-    vector<vector<int>> invalidGraph = {
+    // Create an unweighted graph
+    vector<vector<int>> unweightedGraph = {
+        {0, -1, 0},
+        {-1, 0, -1},
+        {0, -1, 0}};
+    g.loadGraph(unweightedGraph);
+    cout << "Unweighted graph test 2 passed!" << endl;
+}
+
+TEST_CASE("Test isConnected with negative edges") {
+    Graph g;
+    vector<vector<int>> graph = {
         {0, -1, 0},
         {-1, 0, 1},
         {0, 1, 0}};
-    CHECK_THROWS(g.loadGraph(invalidGraph));
+    g.loadGraph(graph);
+    CHECK(Algorithms::isConnected(g) == true); // The graph is connected
+    cout << "isConnected with negative edges test passed!" << endl;
 }
 
+TEST_CASE("Test isBipartite with negative weights") {
+    Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, -1},
+        {1, 0, 1},
+        {-1, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(Algorithms::isBipartite(g) == 0); // The graph is not bipartite due to it has negative weight
+    cout << "isBipartite with negative weights test passed!" << endl;
+}
 
-
+TEST_CASE("Test isContainsCycle for acyclic graph") {
+    Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {0, 0, 1},
+        {0, 0, 0}};
+    g.loadGraph(graph);
+    CHECK(Algorithms::isContainsCycle(g) == false); // There is no cycle
+    cout << "isContainsCycle for acyclic graph test passed!" << endl;
+}
